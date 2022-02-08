009c02139fa88413a0d37709ee3538003bf2c899
Ngô Hữu Mạnh Khanh <39998591+manhkhanhad@users.noreply.github.com>
2022-02-08 09:31:44 +0700

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -8,7 +8,7 @@ import torch
 sys.path.insert(0, 'pix2pixColorization')
 
 
-def pix2pixColorization():
+def pix2pixColorization_unet256():
     gpu_ids = []
     url = "https://github.com/manhkhanhad/ImageRestorationInfer/releases/download/Pretrained/latest_net_G.pth"
 
@@ -18,6 +18,34 @@ def pix2pixColorization():
             output_nc=2,
             ngf=64,
             netG="unet_256",
+            norm="batch",
+            use_dropout=True,
+            init_type="normal",
+            init_gain=0.02,
+            gpu_ids=gpu_ids,
+        )
+    # get device name: CPU or GPU
+    device = (
+        torch.device("cuda:{}".format(gpu_ids[0])) if gpu_ids else torch.device("cpu")
+    )
+
+    state_dict = torch.hub.load_state_dict_from_url(url, progress=True)
+    if hasattr(state_dict, '_metadata'):
+        del state_dict._metadata
+    model.load_state_dict(state_dict)
+    
+    return model
+
+def pix2pixColorization_resnet9b():
+    gpu_ids = []
+    url = "https://github.com/manhkhanhad/ImageRestorationInfer/releases/download/Pretrained/latest_net_G_resnet9b.pth"
+
+
+    model = networks.define_G(
+            input_nc=1,
+            output_nc=2,
+            ngf=64,
+            netG="resnet_9blocks",
             norm="batch",
             use_dropout=True,
             init_type="normal",
